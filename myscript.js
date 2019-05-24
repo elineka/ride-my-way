660be805021854bd562bc4f845c74f899b8c7ac7
Elineka <elinekamaleo@gmail.com>
2019-05-24 16:31:23 +0300

    Modify Javascript file

--- myscript.js
+++ myscript.js
@@ -1,6 +1,5 @@
 
-<script>
-var d = new Date("2019-05-23T12:00:00Z");{
-document.getElementById("moving").innerHTML = "d.";
+
+var d = new Date();{
+document.getElementById("moving").innerHTML = d;
 }
-</script>
