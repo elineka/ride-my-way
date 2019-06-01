eb1d24868186ba4fc2bdbffb1cf820871dcbc5c9
Elineka <elinekamaleo@gmail.com>
2019-06-01 17:48:05 +0300

    Create a ride offer refer #20

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const ride = [
   {
@@ -17,8 +20,14 @@ const ride = [
   },
   {
     id: 3,
-    cartypen: "Nissan Xterra",
+    cartype: "Nissan Xterra",
     pickup: "Tabata",
+    dropoff: "seedspace"
+  },
+  {
+    id: 13,
+    cartypen: "Harrier",
+    pickup: "Posta",
     dropoff: "seedspace"
   }
 ];
@@ -26,28 +35,33 @@ app.get("/api/v1/rides", (req, res) => {
   res.json(ride);
 });
 app.post("/api/v1/rides", (req, res) => {
-  const ride = [
-    {
-      id: 5,
-      cartype: "Nissan Xterra",
-      pickup: "Tabata",
-      dropoff: "seedspace"
-    }
-  ];
-  res.json(ride);
+  let body = req.body;
+  console.log(req.body);
+  let new_ride = {
+    id: body.id,
+    cartype: body.cartype,
+    pickup: body.pickup,
+    dropoff: body.dropoff
+  };
+  ride.push(new_ride);
+  res.status(201).json(ride);
 });
 app.get("/api/v1/rides/:id", (req, res) => {
-  
-for (i = 0; i <= ride.length; i++) { 
-  if(req.params.id==i){
-    res.json(ride[req.params.id]);
+  for (i = 0; i <= ride.length; i++) {
+    if (req.params.id == i) {
+      res.json(ride[req.params.id]);
+    }
   }
-    
-  }
-
 });
-app.post("/api/v1/rides/:id", (req, res) => {
-  res.json(req.params.id);
+app.post("/api/v1/rides/:id/requests", (req, res) => {
+  let userId = req.body.userId;
+  // console.log("user id: "+userId);
+  for (i = 0; i < ride.length; i++) {
+    if (req.params.id == ride[i].id) {
+      ride[i].user = userId;
+      res.status(201).json(ride[i]);
+    }
+  }
 });
 
 console.log("We live ");
