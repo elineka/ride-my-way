c6793f337262a527ef57e1908167b8771cb56ab9
Elineka <elinekamaleo@gmail.com>
2019-06-24 12:30:59 +0300

    Maintain user profile issue #27

--- frontend/src/component/UserProfile.js
+++ frontend/src/component/UserProfile.js
@@ -5,10 +5,12 @@ class UserProfile extends Component {
   render() {
     return (
       <div>
-         <h2><Link to="/" title="Move to home page" className="menulinking">
-        Ride my way
-            </Link></h2>
-       
+        <h2>
+          <Link to="/" title="Move to home page" className="menulinking">
+            Ride my way
+          </Link>
+        </h2>
+        <h2>Ride my way User Profile</h2>
       </div>
     );
   }
